750e3e6217b27ae2df0636c42a9ee48269a308cc
katherine06898 <lz621238@dal.ca>
2020-06-19 11:33:16 +0000

    Added 3 more waypoint goals and changed orientation. The robot can navigate 2 out of 4 goals. after the second goal, it went into rotate recovery mode.

--- src/simple_navigation_goals.cpp
+++ src/simple_navigation_goals.cpp
@@ -16,33 +16,60 @@ int main(int argc, char** argv){
   }
 
   move_base_msgs::MoveBaseGoal goal;
-  //move_base_msgs::MoveBaseGoal goal2;
+  move_base_msgs::MoveBaseGoal goal2;
+  move_base_msgs::MoveBaseGoal goal3;
+  move_base_msgs::MoveBaseGoal goal4;
 
   //we'll send a goal to the robot to move 1 meter forward
-  goal.target_pose.header.frame_id = "map";
+  goal.target_pose.header.frame_id = "base_link";
   goal.target_pose.header.stamp = ros::Time::now();
 
   goal.target_pose.pose.position.x = 1.1;
-  goal.target_pose.pose.orientation.w = 1.0;
+  goal.target_pose.pose.orientation.w = 0.5;
 
   ROS_INFO("Sending goal");
   ac.sendGoal(goal);
 
   ac.waitForResult();    //wait for the first goal to finish
-  
+ 
+  goal2.target_pose.header.frame_id = "base_link";
+  goal2.target_pose.header.stamp = ros::Time::now();
+
+  goal2.target_pose.pose.position.y = 1.1;
+  goal2.target_pose.pose.orientation.w = 1.0;
+
+  ROS_INFO("Sending goal2");
+  ac.sendGoal(goal2);
+
+  ac.waitForResult();    //wait for the first goal to finish
+
+  goal3.target_pose.header.frame_id = "base_link";
+  goal3.target_pose.header.stamp = ros::Time::now();
+
+  goal3.target_pose.pose.position.x = 1.1;
+  goal3.target_pose.pose.orientation.w = 1.5;
+
+  ROS_INFO("Sending goal3");
+  ac.sendGoal(goal3);
+
+  ac.waitForResult();    //wait for the first goal to finish
+
+  goal4.target_pose.header.frame_id = "base_link";
+  goal4.target_pose.header.stamp = ros::Time::now();
+
+  goal4.target_pose.pose.position.y = 1.1;
+  goal4.target_pose.pose.orientation.w = 2.0;
+
+  ROS_INFO("Sending goal4");
+  ac.sendGoal(goal4);
+
+  ac.waitForResult();    //wait for the first goal to finish
+  /*
   if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
     ROS_INFO("Hooray, the base moved 1.1 meter forward");
   else
     ROS_INFO("The base failed to move forward 1.1 meter for some reason");
-
-  //goal2.target_pose.pose.position.y = 1.1;
-  //goal2.target_pose.pose.orientation.w = 1.0;
-
-  //ROS_INFO("Sending goal");
-  //ac.sendGoal(goal2);
-
-  //ac.waitForResult();    //wait for the second goal to finish
-
+  */
   return 0;
 }
 
