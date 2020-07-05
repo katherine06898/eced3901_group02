f44ea53d15393a1a53ae0790c650db40eea854c3
katherine06898 <lz621238@dal.ca>
2020-07-05 11:38:03 +0000

    figured out theextendable code for lawn mowing pattern and changed the param files to less aggressive version.

--- src/lawn_mowing.cpp
+++ src/lawn_mowing.cpp
@@ -2,6 +2,9 @@
 #include <move_base_msgs/MoveBaseAction.h>
 #include <actionlib/client/simple_action_client.h>
 
+#define W 13
+#define P 3
+
 typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
 
 int main(int argc, char** argv){
@@ -15,82 +18,32 @@ int main(int argc, char** argv){
     ROS_INFO("Waiting for the move_base action server to come up");
   }
 
+  int i = 0;
   move_base_msgs::MoveBaseGoal goal;
-  move_base_msgs::MoveBaseGoal goal2;
-  move_base_msgs::MoveBaseGoal goal3;
-  move_base_msgs::MoveBaseGoal goal4;
-  move_base_msgs::MoveBaseGoal goal5;
+  float goal_arrays[W][P] = {{0.3048, 1.524, 0.5}, {0.9144, 1.524, 0.5}, {0.9144, 0.3048, 0.5}, 
+				{1.524, 0.3048, 0.5}, {1.524, 3.9624, 0.5}, {2.1633, 3.9624, 0.5}, 
+				{2.1633, 1.524, 0.5}, {2.7432, 1.524, 0.5}, {2.7432, 3.9624, 0.5}, 
+				{3.3528, 3.9624, 0.5}, {3.3528, 1.524, 0.5}, {3.9624, 1.524, 0.5}, 
+				{3.9624, 3.9624, 0.5} };
 
+  while(i < W){
+	goal.target_pose.header.frame_id = "map";
+  	goal.target_pose.header.stamp = ros::Time::now();
 
-  //we'll send a goal to the robot to move 1 meter forward
-  goal.target_pose.header.frame_id = "map";
-  goal.target_pose.header.stamp = ros::Time::now();
+	goal.target_pose.pose.position.x = goal_arrays[i][0];
+  	goal.target_pose.pose.position.y = goal_arrays[i][1];
+  	goal.target_pose.pose.orientation.w = goal_arrays[i][2];
+	
+  	ROS_INFO("Sending goal");
+  	ac.sendGoal(goal);
 
-  goal.target_pose.pose.position.x = 0.3048;
-  goal.target_pose.pose.position.y = 1.524;
-  goal.target_pose.pose.orientation.w = 0.5;
+ 	ac.waitForResult();
+	
+	//get next goal
+	i++;
 
-  ROS_INFO("Sending goal");
-  ac.sendGoal(goal);
+  }
 
-  ac.waitForResult();    //wait for the first goal to finish
- 
-  //we'll send a goal to the robot to move 1.1 meter upward
-  goal2.target_pose.header.frame_id = "map";
-  goal2.target_pose.header.stamp = ros::Time::now();
-
-  goal2.target_pose.pose.position.x = 0.9144;
-  goal2.target_pose.pose.position.y = 1.524;
-  goal2.target_pose.pose.orientation.w = 0.5;
-
-  ROS_INFO("Sending goal2");
-  ac.sendGoal(goal2);
-
-  ac.waitForResult();    //wait for the second goal to finish
-
-  //we'll send a goal to the robot to move 1.1 meter to the left
-  goal3.target_pose.header.frame_id = "map";
-  goal3.target_pose.header.stamp = ros::Time::now();
-
-  goal3.target_pose.pose.position.x = 0.9144;
-  goal3.target_pose.pose.position.y = 0.3048;
-  goal3.target_pose.pose.orientation.w = 0.5;
-
-  ROS_INFO("Sending goal3");
-  ac.sendGoal(goal3);
-
-  ac.waitForResult();    //wait for the third goal to finish
-
-  //we'll send a goal to the robot to move back to the origin
-  goal4.target_pose.header.frame_id = "map";
-  goal4.target_pose.header.stamp = ros::Time::now();
-
-  goal4.target_pose.pose.position.x = 1.524;
-  goal4.target_pose.pose.position.y = 0.3048;
-  goal4.target_pose.pose.orientation.w = 0.5;
-
-  ROS_INFO("Sending goal4");
-  ac.sendGoal(goal4);
-
-  ac.waitForResult();    //wait for the fourth goal to finish
-
-  goal5.target_pose.header.frame_id = "map";
-  goal5.target_pose.header.stamp = ros::Time::now();
-
-  goal5.target_pose.pose.position.x = 1.524;
-  goal5.target_pose.pose.position.y = 3.9624;
-  goal5.target_pose.pose.orientation.w = 0.5;
-
-  ROS_INFO("Sending goal5");
-  ac.sendGoal(goal5);
-
-  ac.waitForResult();    //wait for the fourth goal to finish
-  /*
-  if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
-    ROS_INFO("Hooray, the base moved 1.1 meter forward");
-  else
-    ROS_INFO("The base failed to move forward 1.1 meter for some reason");
-  */
   return 0;
 }
 
